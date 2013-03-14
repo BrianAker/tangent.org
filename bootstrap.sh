b2022dbf094aa4666dcf0cdff5ccad08ee34f23a
Brian Aker <brian@tangent.org>
2013-03-14 00:41:09 -0700

    Check in tangent.org

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if test -f configure; then make clean; make merge-clean; make distclean; fi;
+
+rm -r -f autom4te.cache
+autoreconf --install --force --verbose -Wall
+./configure
+make
