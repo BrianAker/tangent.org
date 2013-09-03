81b9be0f4de8aa396c0288564e037f32c0201ce6
Brian Aker <brian@tangent.org>
2013-09-03 03:29:40 -0700

    Final fixes

--- docs/include.mk
+++ docs/include.mk
@@ -15,6 +15,16 @@ SPHINXBUILD = @SPHINXBUILD@
 RST=
 RST+= ${SPHINX_BUILDDIR}/index.rst
 RST+= ${SPHINX_BUILDDIR}/about.rst
+STATIC=
+STATIC+= ${SPHINX_BUILDDIR}/_static/TangentOrg.png
+STATIC+= ${SPHINX_BUILDDIR}/_static/drizzle.jpg
+STATIC+= ${SPHINX_BUILDDIR}/_static/favicon.ico
+STATIC+= ${SPHINX_BUILDDIR}/_static/gearman.png
+STATIC+= ${SPHINX_BUILDDIR}/_static/libmemcached.jpg
+STATIC+= ${SPHINX_BUILDDIR}/_static/tangent.jpg
+
+EXTRA_DIST+= $(RST)
+EXTRA_DIST+= $(STATIC)
 
 # Internal variables.
 PAPEROPT_a4     = -D latex_paper_size=a4
